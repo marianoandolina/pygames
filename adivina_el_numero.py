e63989ac4dcc057b54aceeb1c69b4d0c626fcfae
Mariano Andolina <mariano.3gs@gmail.com>
2023-05-25 19:32:47 -0300

    1er juego adivina el numero

---
+++ adivina_el_numero.py
@@ -0,0 +1,34 @@
+from random import randint
+
+print('Bienvenido a adivina el Numero!')
+print('Como es tu nombre?')
+nombre = input()
+print(f'Bueno {nombre}, estoy pensando un numero entre 1 y 40')
+print('Puedes adivinarlo?')
+print('Ingresa el numero que creas que estoy pensado')
+
+intentos = 0
+numero_random = randint(1, 40)
+
+while intentos < 6:
+    
+    numero_usuario = int(input())
+
+    intentos = intentos + 1
+
+    if numero_usuario < numero_random:
+        print('El numero ingresado es menor al que tengo pensado')
+    if numero_usuario > numero_random:
+        print('El numero ingresado es mayor al que tengo pensado') 
+    if numero_usuario == numero_random:
+        print('Adivinaste!')
+        break
+
+if numero_usuario == numero_usuario:
+    print(f'Muy bien {nombre}, adivinaste el numero en {intentos} intentos!! Te felicito')
+
+if numero_usuario != numero_random:
+    print(f'No, el numero que estaba pensando era {numero_random}' )    
+          
+    
+
---
+++ prueba.py
@@ -0,0 +1,5 @@
+print('Hola, mundo')
+print('Bienvenido')
+print('Como te llamas?')
+nombre = input()
+print(f'Hola {nombre}')
