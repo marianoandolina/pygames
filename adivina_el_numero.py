674cb902d220df8ed18c139507927323b89ceddc
Mariano Andolina <mariano.3gs@gmail.com>
2023-05-26 16:53:29 -0300

    Embelleciendo adivina el numero

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,16 +1,23 @@
 from random import randint
+import time
 
 print('Bienvenido a adivina el Numero!')
+print()
 print('Como es tu nombre?')
 nombre = input()
-print(f'Bueno {nombre}, estoy pensando un numero entre 1 y 40')
-print('Puedes adivinarlo?')
+print()
+print(f'Hola {nombre}')
+print()
+time.sleep(2)
+print()
+print('Estoy pensando un numero entre 1 y 40')
+print('Puedes adivinarlo en no mas de 5 intentos?')
 print('Ingresa el numero que creas que estoy pensado')
 
 intentos = 0
 numero_random = randint(1, 40)
 
-while intentos < 6:
+while intentos < 5:
 
     numero_usuario = int(input())
 
@@ -18,15 +25,22 @@ while intentos < 6:
 
     if numero_usuario < numero_random:
         print('El numero ingresado es menor al que tengo pensado')
-    if numero_usuario > numero_random:
+    elif numero_usuario > numero_random:
         print('El numero ingresado es mayor al que tengo pensado')
-    if numero_usuario == numero_random:
+    elif numero_usuario == numero_random:
         print('Adivinaste!')
         break
 
-if numero_usuario == numero_usuario:
-    print(
-        f'Muy bien {nombre}, adivinaste el numero en {intentos} intentos!! Te felicito')
+if numero_usuario == numero_random:
+    print()
+    print(f"Muy bien {nombre}, adivinaste el numero en {intentos} intentos!!")
+    print("******** FELICITACIONES *********")
+    print("Leiste mi pensamiento a la perfeccion")
 
 if numero_usuario != numero_random:
-    print(f'No, el numero que estaba pensando era {numero_random}')
+    print()
+    print('Agotaste la cantidad de intentos!')
+    print()
+    print(f'El numero que estaba pensando era el {numero_random}')
+    print()
+    print("No te preocupes, puedes volver a intentar")
