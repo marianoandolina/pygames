42632b727ba29f900147397cdc4f99881d5ccbdb
Mariano Andolina <mariano.3gs@gmail.com>
2023-06-02 23:29:10 -0300

    Commit ahorcado terminado

--- prueba.py
+++ prueba.py
@@ -1,2 +1,60 @@
-print('spam')
-input()
+# historia = '''Hola, esta es la historia del fantasma de Cantervielle
+# me interesa, porque quiero saber si esta parte se va a imprimir si 
+# le doy un print para que se vea por consola'''
+
+# print(historia)
+
+############################################################################
+
+# animales = ['aguila', 'alce', 'antilope', 'albert']
+
+# print(animales[0])
+
+# print(animales[1])
+
+# print(animales[2])
+
+# print(animales[3])
+
+############################################################################
+
+# animales = ['gato', 'perro', 'antilope', 'lince', 'guepardo']
+# bebidas = ['agua', 'gaseosa', 'te', 'cafe', 'leche']
+# todos= animales + bebidas
+# print(todos)
+
+############################################################################
+
+# print(animales)
+# del animales[0] #Borra el str que se encuentra en el indice[0]
+# print(animales)
+
+###########################################################################
+
+# saludo = "Hola Mundo!"
+# print(saludo.lower())
+# print(saludo.capitalize())
+
+###########################################################################
+
+# marcas = ['samsung', 'motorola', 'phillips', 'apple', 'xiaomi']
+# print(marcas)
+# marcas.append("nike")
+# print(marcas)
+# marcas.append('adidas')
+# print(marcas)
+# marcas.reverse()
+# print(marcas)
+
+##########################################################################
+
+partes_pc = '''mother video cooler hdd ssd m2 parlantes teclado monitor mouse 
+ram fuente auriculares'''.split()
+
+print(partes_pc)
+
+print(partes_pc.index('cooler'))
+
+
+
+
